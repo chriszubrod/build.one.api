c84321f122b5b63c3fedae4afcabaed757260ce1
chris <chris@bchristopher.dev>
2025-04-06 17:10:03 -0500

    Update commit for Azure Docker deployment

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import (
-    Flask,
-    render_template
+    Flask
 )
-from agents import sample_agent
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return "Hello from Docker!"

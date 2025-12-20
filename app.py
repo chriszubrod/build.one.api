490f36028b0946c5510a8c2970779ad3f02c55f6
chris <chris@bchristopher.dev>
2025-12-20 12:45:59 -0600

    Update commit app imports for new modules.

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from modules.auth.api.router import router as auth_api_router
 from modules.auth.web.controller import router as auth_web_router
 from modules.organization.api.router import router as organization_api_router
 from modules.organization.web.controller import router as organization_web_router
+from modules.company.api.router import router as company_api_router
+from modules.company.web.controller import router as company_web_router
 from modules.cost_code.api.router import router as cost_code_api_router
 from modules.cost_code.web.controller import router as cost_code_web_router
 from modules.sub_cost_code.api.router import router as sub_cost_code_api_router
@@ -45,6 +47,8 @@ app.include_router(auth_api_router)
 app.include_router(auth_web_router)
 app.include_router(organization_api_router)
 app.include_router(organization_web_router)
+app.include_router(company_api_router)
+app.include_router(company_web_router)
 app.include_router(cost_code_api_router)
 app.include_router(cost_code_web_router)
 app.include_router(sub_cost_code_api_router)

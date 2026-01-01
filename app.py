8e3f553db87add99b65a2d16c8abdb795b77f152
chris <chris@bchristopher.dev>
2026-01-01 17:35:32 -0600

    Update commit for new module imports.

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ from typing_extensions import Annotated
 
 # Local Imports
 import config
+
+from modules.address.api.router import router as address_api_router
+from modules.address.web.controller import router as address_web_router
+from modules.address_type.api.router import router as address_type_api_router
+from modules.address_type.web.controller import router as address_type_web_router
 from modules.auth.api.router import router as auth_api_router
 from modules.auth.web.controller import router as auth_web_router
 from modules.integration.api.router import router as integration_api_router
@@ -28,11 +33,17 @@ from modules.user_role.api.router import router as user_role_api_router
 from modules.user_role.web.controller import router as user_role_web_router
 from modules.vendor.api.router import router as vendor_api_router
 from modules.vendor.web.controller import router as vendor_web_router
+from modules.vendor_address.api.router import router as vendor_address_api_router
+from modules.vendor_address.web.controller import router as vendor_address_web_router
 from modules.vendor_type.api.router import router as vendor_type_api_router
 from modules.vendor_type.web.controller import router as vendor_type_web_router
+from modules.taxpayer.api.router import router as taxpayer_api_router
+from modules.taxpayer.web.controller import router as taxpayer_web_router
 from modules.role_module.api.router import router as role_module_api_router
 from modules.role_module.web.controller import router as role_module_web_router
 from modules.dashboard.web.controller import router as dashboard_web_router
+
+from integrations.intuit.qbo.auth.api.router import router as intuit_qbo_auth_api_router
 from integrations.sync.api.router import router as sync_api_router
 from integrations.sync.web.controller import router as sync_web_router
 from integrations.intuit.qbo.vendor.api.router import router as qbo_vendor_api_router
@@ -45,6 +56,10 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+app.include_router(address_api_router)
+app.include_router(address_web_router)
+app.include_router(address_type_api_router)
+app.include_router(address_type_web_router)
 app.include_router(auth_api_router)
 app.include_router(auth_web_router)
 app.include_router(integration_api_router)
@@ -65,6 +80,9 @@ app.include_router(user_role_api_router)
 app.include_router(user_role_web_router)
 app.include_router(vendor_api_router)
 app.include_router(vendor_web_router)
+app.include_router(vendor_address_api_router)
+app.include_router(vendor_address_web_router)
+app.include_router(intuit_qbo_auth_api_router)
 app.include_router(sync_api_router)
 app.include_router(sync_web_router)
 app.include_router(qbo_vendor_api_router)
@@ -73,6 +91,8 @@ app.include_router(qbo_client_api_router)
 app.include_router(qbo_client_web_router)
 app.include_router(vendor_type_api_router)
 app.include_router(vendor_type_web_router)
+app.include_router(taxpayer_api_router)
+app.include_router(taxpayer_web_router)
 app.include_router(role_module_api_router)
 app.include_router(role_module_web_router)
 app.include_router(dashboard_web_router)

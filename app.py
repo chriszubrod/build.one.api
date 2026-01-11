cc7163e15b33900ebe5a777f6a8dbd67379a1cf3
chris <chris@bchristopher.dev>
2026-01-11 15:50:07 -0700

    Update commit app master file for new module imports.

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ from modules.address_type.api.router import router as address_type_api_router
 from modules.address_type.web.controller import router as address_type_web_router
 from modules.auth.api.router import router as auth_api_router
 from modules.auth.web.controller import router as auth_web_router
+from modules.customer.api.router import router as customer_api_router
+from modules.customer.web.controller import router as customer_web_router
 from modules.integration.api.router import router as integration_api_router
 from modules.integration.web.controller import router as integration_web_router
 from modules.organization.api.router import router as organization_api_router
@@ -50,6 +52,9 @@ from modules.role_module.web.controller import router as role_module_web_router
 from modules.dashboard.web.controller import router as dashboard_web_router
 from modules.attachment.api.router import router as attachment_api_router
 from modules.attachment.web.controller import router as attachment_web_router
+from modules.bill.api.router import router as bill_api_router
+from modules.bill.web.controller import router as bill_web_router
+from modules.bill_line_item.api.router import router as bill_line_item_api_router
 from modules.taxpayer_attachment.api.router import router as taxpayer_attachment_api_router
 from modules.taxpayer_attachment.web.controller import router as taxpayer_attachment_web_router
 from modules.legal.web.controller import router as legal_web_router
@@ -92,6 +97,8 @@ app.include_router(address_type_api_router)
 app.include_router(address_type_web_router)
 app.include_router(auth_api_router)
 app.include_router(auth_web_router)
+app.include_router(customer_api_router)
+app.include_router(customer_web_router)
 app.include_router(integration_api_router)
 app.include_router(integration_web_router)
 app.include_router(organization_api_router)
@@ -130,6 +137,9 @@ app.include_router(role_module_web_router)
 app.include_router(dashboard_web_router)
 app.include_router(attachment_api_router)
 app.include_router(attachment_web_router)
+app.include_router(bill_api_router)
+app.include_router(bill_web_router)
+app.include_router(bill_line_item_api_router)
 app.include_router(taxpayer_attachment_api_router)
 app.include_router(taxpayer_attachment_web_router)
 app.include_router(legal_web_router)
